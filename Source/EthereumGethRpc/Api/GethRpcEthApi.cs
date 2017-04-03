90de1cc504d63ebcdac2e227a461ef17cfc28eef
Nicolas CLERC <nicolas.clerc@sablefin.net>
2017-04-03 16:01:48 +0200

    .

--- Source/EthereumGethRpc/Api/GethRpcEthApi.cs
+++ Source/EthereumGethRpc/Api/GethRpcEthApi.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace EthereumGethRpc.Api
 {
+
     /// <summary>
     /// Encapsulation of ETH rpc api for Geth
     /// Not intended to be used in a standalone instance (instance created by GethRpcProxy class)
